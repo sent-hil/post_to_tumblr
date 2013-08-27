04473c0a07aa59dcfc447a97afa8cb76322ba83b
sent-hil <me@sent-hil.com>
2013-08-26 22:17:54 -0700

    format fixes

--- main.go
+++ main.go
@@ -24,7 +24,8 @@ func main() {
 	}
 
 	defer func() {
-		if err := logFile.Close(); err != nil {
+		err := logFile.Close()
+		if err != nil {
 			panic(err)
 		}
 	}()
@@ -35,7 +36,6 @@ func main() {
 	}
 
 	var numberOfPosts int = len(postsDir)
-
 	lastFile := postsDir[numberOfPosts-1]
 	fullFileName := fmt.Sprintf("posts/%v", lastFile.Name())
 
@@ -44,64 +44,11 @@ func main() {
 		panic(err)
 	}
 
-	from := flag.String("f", "", "Gmail username")
-	password := flag.String("p", "", "Gmail password")
-	flag.Parse()
-
 	joinedTitle := extractTitleFromFileName(lastFile.Name())
-
-	err = sendEmail(*from, *password, joinedTitle, string(contents))
+	err = sendEmail(joinedTitle, string(contents))
 	if err != nil {
 		panic(err)
 	}
-}
-
-func extractTitleFromFileName(fileName string) string {
-	var nameWithoutExtension string
-	var joinedTitle string
-
-	splitFileNames := strings.Split(fileName, ".")
-	nameWithoutExtension = splitFileNames[0]
-
-	splitFileNames = strings.Split(nameWithoutExtension, "_")
-	for _, str := range splitFileNames[1:] {
-		joinedTitle += fmt.Sprintf("%s ", str)
-	}
-
-	log.Printf("parsed title: %s from file %s", joinedTitle, fileName)
-
-	return joinedTitle
-}
-
-func sendEmail(from, password, subject, body string) error {
-	// tumblr looks for !m in subject so it can parse the email as markdown
-	subject = subject + " !m"
-
-	header := map[string]string{
-		"From":         from,
-		"To":           tumblrEmail,
-		"Subject":      subject,
-		"MIME-Version": "1.0",
-	}
-
-	message := ""
-	for k, v := range header {
-		message += fmt.Sprintf("%s: %s\r\n", k, v)
-	}
-
-	message = fmt.Sprintf("%s\n%s", message, body)
-	to := []string{tumblrEmail}
-
-	auth := smtp.PlainAuth("", from, password, "smtp.gmail.com")
-
-	err := smtp.SendMail("smtp.gmail.com:587", auth, from, to, []byte(message))
-	if err != nil {
-		return err
-	}
-
-	log.Printf("email sent to %v", tumblrEmail)
-
-	return nil
 }
 
 func readOrCreateLogFile() (*os.File, error) {
@@ -133,4 +80,54 @@ func readFromPostDir() ([]os.FileInfo, error) {
 	log.Printf("found %v posts in dir %s", len(postsDir), postsDirName)
 
 	return postsDir, nil
+}
+
+func extractTitleFromFileName(fileName string) string {
+	var nameWithoutExtension string
+	var joinedTitle string
+
+	splitFileNames := strings.Split(fileName, ".")
+	nameWithoutExtension = splitFileNames[0]
+
+	splitFileNames = strings.Split(nameWithoutExtension, "_")
+	for _, str := range splitFileNames[1:] {
+		joinedTitle += fmt.Sprintf("%s ", str)
+	}
+
+	log.Printf("parsed title: %s from file %s", joinedTitle, fileName)
+
+	return joinedTitle
+}
+
+func sendEmail(subject, body string) error {
+	from := flag.String("f", "", "Gmail username")
+	password := flag.String("p", "", "Gmail password")
+	flag.Parse()
+
+	// tumblr looks for !m in subject so it can parse the email as markdown
+	subject = subject + " !m"
+
+	header := map[string]string{
+		"From":         *from,
+		"To":           tumblrEmail,
+		"Subject":      subject,
+		"MIME-Version": "1.0",
+	}
+
+	var message string
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+
+	message = fmt.Sprintf("%s\n%s", message, body)
+	to := []string{tumblrEmail}
+	auth := smtp.PlainAuth("", *from, *password, "smtp.gmail.com")
+	err := smtp.SendMail("smtp.gmail.com:587", auth, *from, to, []byte(message))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("email sent to %v", tumblrEmail)
+
+	return nil
 }
